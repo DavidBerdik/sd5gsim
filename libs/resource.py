d2cb42640449b6e6566d4a25a0e99dc4dc06c848
David Berdik <berdikgit@theprogrammersworld.net>
2020-11-07 16:02:31 -0500

    Replaced all use of spaces for indentation with tabs

--- libs/resource.py
+++ libs/resource.py
@@ -2,11 +2,11 @@ import uuid
 
 
 class resource:
-    def __init__(self, **kwargs):
-        self.res_id = "r_" + str(uuid.uuid4())
-        self.alloc_ch = None
-        self.alloc_ant = None
-        self.alloc_time = None
-        self.alloc_power = None
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+	def __init__(self, **kwargs):
+		self.res_id = "r_" + str(uuid.uuid4())
+		self.alloc_ch = None
+		self.alloc_ant = None
+		self.alloc_time = None
+		self.alloc_power = None
+		for key, value in kwargs.items():
+			setattr(self, key, value)
--- libs/v_node.py
+++ libs/v_node.py
@@ -5,28 +5,28 @@ import libs.resource as resource
 
 
 class v_node:
-    def __init__(self, **kwargs):
-        self.v_node_id = "v_" + str(uuid.uuid4())
-        self.active = False
-        self.alloc_res_list = []
-        self.parent_nd = None
-        self.parent_bs = None
+	def __init__(self, **kwargs):
+		self.v_node_id = "v_" + str(uuid.uuid4())
+		self.active = False
+		self.alloc_res_list = []
+		self.parent_nd = None
+		self.parent_bs = None
 
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+		for key, value in kwargs.items():
+			setattr(self, key, value)
 
-    def add_res(self, **kwargs):  # kwargs = {"channel": channel,"ant": ant, "power": power, "time": time}
-        res = resource(**kwargs)
-        self.alloc_res_list.append(res)
+	def add_res(self, **kwargs):  # kwargs = {"channel": channel,"ant": ant, "power": power, "time": time}
+		res = resource(**kwargs)
+		self.alloc_res_list.append(res)
 
-    def activate(self):
-        self.active = True
+	def activate(self):
+		self.active = True
 
-    def deactivate(self):
-        self.active = False
+	def deactivate(self):
+		self.active = False
 
-    def request_res(self):
-        pass
+	def request_res(self):
+		pass
 
-    def vacate_res(self):
-        pass
+	def vacate_res(self):
+		pass
